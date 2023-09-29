6a1b153e25a9e1f612196a06a99e0e806301290e
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2023-09-29 22:25:37 +0500

    change visible of check for Login and Register

--- FanurApp/ViewModels/Account/RegisterVM.cs
+++ FanurApp/ViewModels/Account/RegisterVM.cs
@@ -1,8 +1,13 @@
-﻿namespace FanurApp.ViewModels.Account;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FanurApp.ViewModels.Account;
 
 public class RegisterVM
 {
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
